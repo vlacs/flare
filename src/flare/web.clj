72fc69723f08ae1a9ec34fc4a0c2f5327c773e83
Matt Oquist <moquist@majen.net>
2014-06-03 16:39:06 -0400

    make flare helmsman-definition support receipt of 'system

--- src/flare/web.clj
+++ src/flare/web.clj
@@ -1,6 +1,6 @@
 (ns flare.web)
 
-(def helmsman-definition
-  "Main helmsman definition"
-  [[:put "/subscribe" :a #_flare/subscribe]
-   [:put "/unsubscribe" :b #_flare/unsubscribe]])
+(defn helmsman-definition [system]
+  (let [db-conn (:db-conn system)]
+    [[:put "/subscribe" :a #_flare/subscribe]
+     [:put "/unsubscribe" :b #_flare/unsubscribe]]))
