a2f901cf008328e97a2c278c7c226ed57d7180da
Jon Doane <jdoane@vlacs.org>
2014-06-06 11:53:12 -0400

    Updated project.clj with data that wasn't already populated.

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject org.vlacs/flare "0.1.0-SNAPSHOT"
-  :description "TODO"
-  :url "TODO"
-  :license {:name "TODO: Choose a license"
-            :url "http://choosealicense.com/"}
+(defproject org.vlacs/flare "0.1.0"
+  :description "Flare is an event bus that uses Datomic to store events and
+               data associated with those events so other applications can
+               subscribe to events where Flare will asyncronously call the
+               other applications API that they provide to Flare."
+  :url "https://www.github.com/vlacs/flare"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.datomic/datomic-free "0.9.4766.11"]
                  [org.vlacs/hatch "0.2.0"]
