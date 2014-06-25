0644d50207e658755c10adb868a0e63a7bd662cf
Jon Doane <jdoane@vlacs.org>
2014-06-25 11:37:36 -0400

    Added default config file. Updated test.sh to copy missing config file. Updated .gitignore to not version flare-config.edn.

--- test.sh
+++ test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-lein do voom freshen, voom build-deps, test
+lein voom freshen
+lein voom build-deps
+
+if [ ! -f flare-conf.edn ]; then
+  echo "\nI didn't find your flare-conf.edn, I'll copy it for you.\n"
+  cp flare-conf-dist.edn flare-conf.edn
+fi
+
