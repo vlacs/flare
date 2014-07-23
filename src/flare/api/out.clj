0a4e347ee4e56f6fd0a1e7a4a48f15de5cf6a87a
Jon Doane <jdoane@vlacs.org>
2014-07-23 09:53:28 -0400

    Updated outgoing flare api with a basic http call wrapper for call-api!.

--- src/flare/api/out.clj
+++ src/flare/api/out.clj
@@ -1,24 +1,12 @@
 (ns flare.api.out
-  (:require [datomic.api :as d]
-            [hatch]
-            [flare.db]
-            [flare.db.queries :as queries]
-            [flare.db.rules :as rules]
-            [flare.util :as util]
-            [flare.event :as event]
+  (:require 
             [org.httpkit.client :as http]
             [taoensso.timbre :as timbre]))
-
-(defn default-outgoing-fn!
-  [message]
-  (timbre/debug "Outgoing-fn! message:" message))
 
 (def default-opts
   {:timeout 60
    :user-agent "org.vlacs/flare/http-kit"
-   :method :post
-   :headers {:Accept "application/json"
-             :Accept-Charset "utf-8"
+   :headers {:Accept-Charset "utf-8"
              :Cache-Control "no-cache"
              :Connection "keep-alive"
              :X-Requested-With "clojure/http-kit"}
@@ -26,26 +14,9 @@
    :insecure? false
    :follow-redirects false})
 
-(defn prep-options
-  [method url auth-token payload]
-  (-> default-opts
-      (assoc-in [:headers :Authorization] auth-token)
-      (assoc :body payload)
-      (assoc :url url)))
-
-#_
-(defn call-api
-  "Remote API to call to inform about an event.
-  method is a subscription http-method.
-  url is a good ol web address to the api you're calling.
-  auth-token is our super awesome security validation token.
-  outgoing-format is a subscription.format enum value.
-  payload is what you're telling the remote system."
-  [method url auth-token outgoing-format payload]
-  (http/request
-    (prep-options
-      (method method-translation)
-      url
-      auth-token
-      (prep-payload outgoing-format payload))))
+(defn call-api!
+  [request-map]
+  (timbre/info (str "API call (" (name (:method request-map))
+                    ") to: " (:url request-map)))
+  (http/request request-map))
 
